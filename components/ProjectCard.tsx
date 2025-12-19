1576919714a1e8ee819de8cc1328fb9d4e746a6a
nicktperez <nicktperez@gmail.com>
2025-12-19 15:21:10 -0800

    refactor: update Project type import path

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { Project } from '../types/project';
+import { Project } from '@/types';
 import { clsx } from 'clsx';
 import { Trash2 } from 'lucide-react';
 
